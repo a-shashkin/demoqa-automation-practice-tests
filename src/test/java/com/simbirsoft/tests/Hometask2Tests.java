55655afa9c479a003c7bfdde3dcadf927c688867
a-shashkin <caezar1993@gmail.com>
2022-04-25 18:44:00 +0400

    Hometask2Tests.java updated, did some refactoring. This is final version.

--- src/test/java/com/simbirsoft/tests/Hometask2Tests.java
+++ src/test/java/com/simbirsoft/tests/Hometask2Tests.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import java.io.File;
+import java.lang.reflect.Array;
 import java.time.Duration;
+import java.util.Arrays;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -28,28 +30,42 @@ public class Hometask2Tests {
         String email = "emaildoesnotexist@nodomain.com";
         String mobilePhone = "7917624862";
         String currentAddress = "14 Imaginary St., Nevercity, Landdoesnotexist";
-        String searchMaths = "Ma";
-        String searchComputerScience = "Co";
-        String searchEnglish = "E";
+        String searchMaths = "Maths";
+        String searchComputerScience = "Computer Science";
+        String searchEnglish = "English";
 
         open("https://demoqa.com/automation-practice-form");
         $("input#firstName").setValue(firstName);
         $("input#lastName").setValue(lastName);
         $("input#userEmail").setValue(email);
         $x("//label[@for='gender-radio-1']").click();
+        String selectedGender = $x("//input[@id='gender-radio-1']").getValue();
         $("input#userNumber").setValue(mobilePhone);
         $("input#dateOfBirthInput").click();
         $x("//select[@class='react-datepicker__month-select']").selectOptionByValue("4");
         $x("//select[@class='react-datepicker__year-select']").selectOptionByValue("1992");
         $x("//div[@class='react-datepicker__day react-datepicker__day--011']").click();
+        /*
+        В этом месте я думал, что проверить надо таким образом: фрагмент названия предмета вставить, а потом нажать на элементе из списка.
+        Благо в списке подходящих по фильтру элементов у div'ов уникальные id. Так совпало, что нужный предмет всегда шёл первым.
+        Когда по другому поводу смотрел разбор ДЗ, разузнал, что можно гораздо проще.
+        Свой старый код закомментил для истории.
+
         $("input#subjectsInput").setValue(searchMaths);
         $x("//div[@id='react-select-2-option-0']").click();
         $("input#subjectsInput").setValue(searchComputerScience);
         $x("//div[@id='react-select-2-option-0']").click();
         $("input#subjectsInput").setValue(searchEnglish);
-        $x("//div[@id='react-select-2-option-0']").click();
+        $x("//div[@id='react-select-2-option-0']").click();*/
+
+        $("input#subjectsInput").setValue(searchMaths).pressEnter();
+        $("input#subjectsInput").setValue(searchComputerScience).pressEnter();
+        $("input#subjectsInput").setValue(searchEnglish).pressEnter();
         $x("//label[@for='hobbies-checkbox-2']").click();
         $x("//label[@for='hobbies-checkbox-3']").click();
+        String selectedHobbies = $x("//label[@for='hobbies-checkbox-2']").getText()
+                + ", "
+                + $x("//label[@for='hobbies-checkbox-3']").getText();
         File file = new File("src/test/java/com/simbirsoft/tests/7dBulQtN5gc.jpg");
         $x("//input[@id='uploadPicture']").uploadFile(file);
         $("textarea#currentAddress").setValue(currentAddress);
@@ -64,17 +80,18 @@ public class Hometask2Tests {
         $x("//table[@class='table table-dark table-striped table-bordered table-hover']").shouldHave(
                 text(firstName + " " + lastName),
                 text(email),
-                text("Male"),
+                text(selectedGender),
                 text(mobilePhone),
-                text("11 May,1992"),
-                text("Maths, Computer Science, English"),
-                text("Reading, Music"),
+                text("11 May,1992"), // через переменную не удалось сделать, т.к. если брать значение, там вместо запятой ещё пробел
+                        // и я не смог найти рабочий способ заменить второй пробел на запятую
+                text(searchMaths + ", " + searchComputerScience + ", " + searchEnglish),
+                text(selectedHobbies),
                 text("7dBulQtN5gc.jpg"),
                 text(currentAddress),
-                text("NCR Noida")
+                text("NCR Noida") // тоже хотел сделать String, где два значения из выпадашек склеились бы в один String
+                                  // но не нашёл такого в видео с разбором :(
         );
         $("button#closeLargeModal").shouldBe(visible);
-
-        $("textarea#currentAddress").setValue(currentAddress);
     }
 }
+
